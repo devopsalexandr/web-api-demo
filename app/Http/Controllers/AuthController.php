83d86d95711c31090060c3aff19c2dc9fe69ee1f
devopsalexandr <devopsalexandr@gmail.com>
2021-09-20 16:45:53 +0200

    login method created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,14 +3,17 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginFromRequest;
 use App\Http\Requests\RegisterFormRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
 
-    public function register(RegisterFormRequest $request)
+    public function register(RegisterFormRequest $request): UserResource
     {
         $user = User::create([
             'first_name' => $request->first_name,
@@ -20,6 +23,24 @@ class AuthController extends Controller
             'slug' => mb_strtolower($request->first_name."-".$request->last_name)
         ]);
 
+        $token = $user->createToken("application")->plainTextToken;
+
+        return (new UserResource($user))->additional([
+            'meta' => [
+                'token' => $token
+            ]
+        ]);
+    }
+
+    public function login(LoginFromRequest $request, User $user): UserResource
+    {
+        $user = $user->where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
         $token = $user->createToken("application")->plainTextToken;
 
         return (new UserResource($user))->additional([
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/test', [\App\Http\Controllers\AuthController::class, "index"]);
 
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {

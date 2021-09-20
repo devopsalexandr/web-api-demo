a9985881f7078650a2bd583eb5e70b6e9098413c
devopsalexandr <devopsalexandr@gmail.com>
2021-09-20 16:46:42 +0200

    routing v1 fixed

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-//Route::prefix('v1')->group(function () {
-//    Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
-//});
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+});
 
 Route::post('/test', [\App\Http\Controllers\AuthController::class, "index"]);
 
-Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
-Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+
 
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
